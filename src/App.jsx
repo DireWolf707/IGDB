ccef5661ea39fd5773279bdec001b714e117de97
DireWolf707 <rahulsood707@gmail.com>
2023-03-05 23:37:50 +0530

    added component render logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from "react"
-import { Box, Stack } from "@mui/system"
+import { Stack } from "@mui/system"
 import SeacrhBar from "./components/SeacrhBar"
+import GameSearchList from "./components/GameSearchList"
+import GamePopularList from "./components/GamePopularList"
+import GameDetail from "./components/GameDetail"
+import { useSelector } from "react-redux"
 
 const App = () => {
+  const { searchTerm, selectedGameId } = useSelector((store) => store.data)
+
   return (
-    <Stack sx={{ height: "100vh", width: "100vw", bgcolor: "teal" }}>
+    <Stack sx={{ height: "100vh", width: "100vw", bgcolor: "#000" }}>
       <SeacrhBar />
+      {searchTerm ? <GameSearchList searchTerm={searchTerm} /> : <GamePopularList />}
+      {selectedGameId && <GameDetail gameId={selectedGameId} />}
     </Stack>
   )
 }
