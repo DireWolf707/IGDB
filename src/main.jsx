df84e084673397b4867be07af9f148b2b01843ad
DireWolf707 <rahulsood707@gmail.com>
2023-03-05 23:36:42 +0530

    added store provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,11 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./index.css"
+import { Provider as StoreProvider } from "react-redux"
+import { store } from "./store"
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <StoreProvider store={store}>
+    <App />
+  </StoreProvider>
+)
