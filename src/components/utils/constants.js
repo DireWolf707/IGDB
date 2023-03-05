a8689180a1eb378ddeb83458c05f7a2e41fd83f2
DireWolf707 <rahulsood707@gmail.com>
2023-03-05 23:38:05 +0530

    added svgs

---
+++ src/components/utils/constants.js
@@ -0,0 +1,8 @@
+export const platforms = {
+  "pc": "/pc.svg",
+  "xbox-series-x": "/xbox-x.svg",
+  playstation3: "/ps3.svg",
+  playstation4: "/ps4.svg",
+  playstation5: "/ps5.svg",
+  "nintendo-switch": "/switch.svg",
+}
